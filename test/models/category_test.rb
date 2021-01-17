b905d5fa614059a86991d953408d87b7569148ef
tolves <tov.sharing@gmail.com>
2021-01-17 14:29:34 +0000

    test examples

---
+++ test/models/category_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "should save category with meta_data = array" do
+    assert_difference 'Category.count' do
+      category = Category.new
+      category.title = 'test category'
+      category.meta_data = %w[aaa bbb]
+      category.save
+    end
+  end
+
+  test "should save category with meta_data = string" do
+    assert_difference 'Category.count' do
+      category = Category.new
+      category.title = 'test2'
+      category.meta_data = 'aaa, bbb'
+      category.save
+    end
+  end
+end
