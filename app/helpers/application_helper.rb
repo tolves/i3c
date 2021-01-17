9feddc6109ae1039a6afadbd8d0e3d9408b3030c
tolves <tov.sharing@gmail.com>
2021-01-17 20:26:50 +0000

    nav partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,17 @@ module ApplicationHelper
     end
     doc.to_html.html_safe
   end
+
+  def nav
+    content_tag(:ul, class: "nav nav-tabs justify-content-end") do
+      yield
+    end
+  end
+
+  def nav_link(text, path)
+    options = current_page?(path) ? { class: "nav-link active" } : { class: "nav-link" }
+    content_tag(:li, { class: "nav-item" }) do
+      link_to text, path, options
+    end
+  end
 end
