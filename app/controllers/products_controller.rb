125b27cd1020446f640fbe39b2c1acb09b9b7531
tolves <tov.sharing@gmail.com>
2021-01-18 15:20:44 +0000

    product view: show, delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,26 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(params_product)
+      redirect_to @product
+    else render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path
+  end
+
   private
 
   def params_product
