c7e43e7f0b41ef404fd54f04cd38048c6c2f36b7
tolves <tov.sharing@gmail.com>
2021-01-18 19:48:48 +0000

    fix bug: add new cant load at first time (set link method: :get to force reload page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
   end
 
   def nav_link(text, path)
-    options = current_page?(path) ? { class: "nav-link active disabled" } : { class: "nav-link" }
+    options = current_page?(path) ? { class: "nav-link active disabled" } : { class: "nav-link", method: :get }
     content_tag(:li, { class: "nav-item" }) do
       link_to text, path, options
     end
