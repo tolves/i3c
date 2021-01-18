21a1d75dd05695a11c42518c14f26b075203c4a4
tolves <tov.sharing@gmail.com>
2021-01-18 15:20:32 +0000

    category view: show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,11 +21,23 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
 
+    if @category.update(params_category)
+      redirect_to @category
+    else render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
   end
 
   def meta_data
