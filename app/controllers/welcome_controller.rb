68865792f5f4ad101398cbc6a0a6e209cb1ac826
tolves <tov.sharing@gmail.com>
2021-01-21 16:37:25 +0000

    ajax save products to cart

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,16 @@
 class WelcomeController < ApplicationController
   # before_action :authenticate_user!
   def index
+    @cart = Cart.new
+    @categories = Category.all
+  end
 
+  def ajax_session
+    product = Product.find(params[:id])
+    session[:cart] ||= []
+    session[:cart].append product
+    respond_to do |format|
+      format.json { render json: product.to_json }
+    end
   end
 end
