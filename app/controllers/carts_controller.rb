5215ca36b7cdf22a06ad820135111e35887143ab
tolves <tov.sharing@gmail.com>
2021-01-21 22:14:48 +0000

    carts: create carts, merge session[:cart] to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,25 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :create_cart
+
   def new
 
+  end
+
+  def show
+    session[:cart].each do |index, product|
+      next if current_user.cart.lists.exists?(product_id: product['id'])
+
+      ActiveRecord::Base.connected_to(role: :writing) do
+        current_user.cart.lists.create!(product_id: product['id'], quantity: 0)
+      end
+    end
+    @cart = current_user.cart.lists
+  end
+
+  private
+
+  def create_cart
+    ActiveRecord::Base.connected_to(role: :writing) { current_user.create_cart! } if current_user.cart.blank?
   end
 end
