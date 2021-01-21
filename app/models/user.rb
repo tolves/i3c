9a17c25d3333d71e7ab6155fd7f61a50136d9acf
tolves <tov.sharing@gmail.com>
2021-01-21 16:57:09 +0000

    user has one delivery address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable #, :recoverable, :rememberable, :validatable
   has_one :cart
-  has_many :addresses
+  has_one :address
   has_many :orders
 end
