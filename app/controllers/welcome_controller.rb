2c1d58df940b0da932df13db8c10c2cb11c70a89
tolves <tov.sharing@gmail.com>
2021-01-21 21:26:13 +0000

    session cart optimise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   # before_action :authenticate_user!
+  before_action :initialize_cart
+  #
+  # def initialize
+  #   # session[:cart]
+  # end
   def index
     @cart = Cart.new
     @categories = Category.all
@@ -7,10 +12,15 @@ class WelcomeController < ApplicationController
 
   def ajax_session
     product = Product.find(params[:id])
-    session[:cart] ||= []
-    session[:cart].append product
+    session[:cart][product.category.title] = product
     respond_to do |format|
       format.json { render json: product.to_json }
     end
+  end
+
+  private
+
+  def initialize_cart
+    session[:cart] ||= {}
   end
 end
