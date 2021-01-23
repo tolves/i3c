f56e5ec986d21659a44349a1759601ce642d9cdd
tolves <tov.sharing@gmail.com>
2021-01-23 16:45:06 +0000

    change js functions position

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,60 +1,3 @@
-$(document).ready(function () {
-    // I dont understand why funtion showFrom isn't defined in js.
-    // but window.showFrom worked, but it worked
-    // window.showForm = function (id, width, title) {
-    //     var el = $('#' + id).first();
-    //     if (el.length === 0 || el.is(':visible')) {
-    //         return;
-    //     }
-    //     if (!title) title = el.find('h3.title').text();
-    //     // moves existing modals behind the transparent background
-    //     $(".modal").css('zIndex', 99);
-    //     el.dialog({
-    //         width: width,
-    //         modal: true,
-    //         resizable: false,
-    //         dialogClass: 'modal',
-    //         title: title
-    //     }).on('dialogclose', function () {
-    //         $(".modal").css('zIndex', 101);
-    //     });
-    //     el.find("input[type=text], input[type=submit]").first().focus();
-    // }
-
-    $('select#product_category_id').change(function () {
-        meta_data($(this).val());
-    });
-
-    $('div.selected_product').click(function () {
-        show_products($(this).attr("value"));
-    });
-
-    paypal.Buttons().render('#paypal-button-container');
-
-    // paypal.Buttons({
-    //     createOrder: function(data, actions) {
-    //         // This function sets up the details of the transaction, including the amount and line item details.
-    //         return actions.order.create({
-    //             purchase_units: [{
-    //                 amount: {
-    //                     value: '0.01'
-    //                 }
-    //             }]
-    //         });
-    //     },
-    //     onApprove: function(data, actions) {
-    //         // This function captures the funds from the transaction.
-    //         return actions.order.capture().then(function(details) {
-    //             // This function shows a transaction success message to your buyer.
-    //             alert('Transaction completed by ' + details.payer.name.given_name);
-    //         });
-    //     }
-    // }).render('#paypal-button-container');
-    //This function displays Smart Payment Buttons on your web page.
-
-    console.log("custom js file loaded");
-});
-
 function meta_data(id) {
     // var data = {category_id: $(this).val()}
     Rails.ajax({
@@ -120,3 +63,60 @@ function select_product(product_id) {
         }
     })
 }
+
+$(document).ready(function () {
+    // I dont understand why funtion showFrom isn't defined in js.
+    // but window.showFrom worked, but it worked
+    // window.showForm = function (id, width, title) {
+    //     var el = $('#' + id).first();
+    //     if (el.length === 0 || el.is(':visible')) {
+    //         return;
+    //     }
+    //     if (!title) title = el.find('h3.title').text();
+    //     // moves existing modals behind the transparent background
+    //     $(".modal").css('zIndex', 99);
+    //     el.dialog({
+    //         width: width,
+    //         modal: true,
+    //         resizable: false,
+    //         dialogClass: 'modal',
+    //         title: title
+    //     }).on('dialogclose', function () {
+    //         $(".modal").css('zIndex', 101);
+    //     });
+    //     el.find("input[type=text], input[type=submit]").first().focus();
+    // }
+
+    $('select#product_category_id').change(function () {
+        meta_data($(this).val());
+    });
+
+    $('div.selected_product').click(function () {
+        show_products($(this).attr("value"));
+    });
+
+    paypal.Buttons().render('#paypal-button-container');
+
+    // paypal.Buttons({
+    //     createOrder: function(data, actions) {
+    //         // This function sets up the details of the transaction, including the amount and line item details.
+    //         return actions.order.create({
+    //             purchase_units: [{
+    //                 amount: {
+    //                     value: '0.01'
+    //                 }
+    //             }]
+    //         });
+    //     },
+    //     onApprove: function(data, actions) {
+    //         // This function captures the funds from the transaction.
+    //         return actions.order.capture().then(function(details) {
+    //             // This function shows a transaction success message to your buyer.
+    //             alert('Transaction completed by ' + details.payer.name.given_name);
+    //         });
+    //     }
+    // }).render('#paypal-button-container');
+    //This function displays Smart Payment Buttons on your web page.
+
+    console.log("custom js file loaded");
+});
