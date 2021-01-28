037cb7095a1a69a73df9c7f83e0f4310566d36b2
tolves <tov.sharing@gmail.com>
2021-01-28 16:28:46 +0000

    remove useless index

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,11 +2,7 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
   load_and_authorize_resource :admin
   layout "admin"
-  def index
-    @out = Admin.first
-  end
 
   def show
-    @out = Admin.first
   end
 end
