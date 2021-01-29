da83cd78c5fe1693940909eb3a32fa8831c19813
tolves <tov.sharing@gmail.com>
2021-01-29 00:22:02 +0000

    inbound controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,6 +4,7 @@ class Admin::ProductsController < ApplicationController
   layout "admin"
 
   def index
+    @products = @products.order(updated_at: :desc)
   end
 
   def new
@@ -33,9 +34,24 @@ class Admin::ProductsController < ApplicationController
     redirect_to admin_products_path
   end
 
+  def inbound
+    @inbound = @product.inbound ? @product.inbound : @product.build_inbound
+    if params[:inbound]
+      ActiveRecord::Base.connected_to(role: :writing) do
+        @inbound.update! params_inbound
+      end
+      flash[:success] = 'You has successfully add inbound data'
+      redirect_to admin_products_path
+    end
+  end
+
   private
 
   def params_product
     params.require(:product).permit(:title, :category_id, :description, :band, :price, :image, :content, :meta_data => {})
+  end
+
+  def params_inbound
+    params.require(:inbound).permit(:quantity, :cost, :note)
   end
 end
