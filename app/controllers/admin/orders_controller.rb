45e051a310ba3343a3f72627842aa840c3432b98
tolves <tov.sharing@gmail.com>
2021-01-29 00:28:22 +0000

    order shipment method

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,12 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
   load_and_authorize_resource :order
   layout "admin"
+
   def index
+
+  end
+
+  def shipment
+
   end
 end
