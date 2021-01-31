ece31d36c40eb3cec2358ce42b7c4d04984005e5
tolves <tov.sharing@gmail.com>
2021-01-31 01:00:44 +0000

    get all model histories

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   connects_to database: { writing: :primary, reading: :primary_replica }
   has_paper_trail
+
+  def self.history
+    PaperTrail::Version.where("item_type = ?", self).order(created_at: :desc)
+  end
 end
